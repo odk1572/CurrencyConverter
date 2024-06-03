cf37779c0e630ec2ba2e0992f415196181f51bb2
Om Kariya <161995427+odk1572@users.noreply.github.com>
2024-06-03 12:39:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
                           amount={convertedAmount}
                           currrencyOption={object}
                           onCurrencyChange={(currency)=>setTo(currency)}
-                          selectCurrency={from}
+                          selectCurrency={to}
                           amountDisable
    
                         />
